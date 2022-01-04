230db19daab5883b7f4b48822744445f052caf71
vopravil <97061527+vopravil@users.noreply.github.com>
2022-01-04 18:19:58 +0100

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vystup1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vystup1
+            // 
+            this.vystup1.AcceptsReturn = true;
+            this.vystup1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.vystup1.Font = new System.Drawing.Font("WarHeliosCondCBold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vystup1.Location = new System.Drawing.Point(254, 58);
+            this.vystup1.Margin = new System.Windows.Forms.Padding(5);
+            this.vystup1.MaxLength = 120;
+            this.vystup1.Multiline = true;
+            this.vystup1.Name = "vystup1";
+            this.vystup1.Size = new System.Drawing.Size(329, 337);
+            this.vystup1.TabIndex = 0;
+            this.vystup1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button2.Location = new System.Drawing.Point(57, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "start game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(51, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 36);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "tah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.vystup1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vystup1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+
+        Random r = new Random();
+        int rInt;
+        int pocetX = 0;
+        int pocetO = 0;
+        int vyhryX;
+        int vyhryO;
+        int celkemX = 0;
+        int celkemO = 0;
+        int yminus = 0;
+        int yminusTwo = 0;
+
+        bool hra = true;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[,] pole =
+        {
+
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"},
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}, 
+            {"H","H","H","H","H","H","H","H","H","H"}
+           
+
+
+
+        };
+
+      
+        
+
+        public void CheckKdoHraje()
+        {
+            if (hra == true)
+            {
+
+          
+            int rInt = r.Next(1,3);
+            if (rInt == 1)
+            {
+                label1.Text = "Hraje X";
+                   
+                }
+            else
+            {
+                label1.Text = "Hraje O";
+                  
+
+                }
+            }
+
+        }
+
+
+
+
+
+
+
+
+        public void startGame()
+        {
+            string newLine = Environment.NewLine;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+
+                    vystup1.Text += pole[i, j];
+                    
+
+                }
+                vystup1.Text += ""+newLine ;
+
+            }
+        }
+
+        public void gameOver()
+        {
+            
+
+            if( pocetX == 5)
+            {
+                vystup1.Text = "X win";
+                vyhryX += 1;
+                hra = true;
+
+            }
+            else if ( pocetO == 5)
+            {
+                vystup1.Text = "O win";
+                vyhryO += 1;
+                hra = true;
+            }
+          
+            
+        }
+
+
+        public void checkRows()
+        {
+           
+            for (int i = 0; i < 10; i++)
+            {
+                pocetX = 0;
+                pocetO = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (pole[i, j] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+
+                    if (pole[i, j] != "@")
+                    {
+                        if (pole[i, j] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[i, j] == "O")
+                        {
+                            pocetO += 1;
+                        }
+                       
+
+                        gameOver();
+                    }
+                }
+            }
+        
+        }
+
+        public void checkColomns()
+        {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                pocetX = 0;
+                pocetO = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (pole[j, i] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+                    if (pole[j, i] != "@")
+                    {
+
+                        if (pole[j, i] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[j, i] == "O")
+                        {
+                            pocetO += 1;
+                        }
+                        gameOver();
+                    }
+                }
+            }
+           
+
+        }
+
+
+       public void checkDiagonalOne()
+        {
+
+                for (int i = 0; i < 10; i++)
+            {
+
+                pocetX = 0;
+                pocetO = 0;
+
+
+                for (int j = 0; j < i+1; j++)
+                {
+                    if (pole[i - j, j] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+
+                    if (pole[i - j, j] != "@")
+                    {
+
+
+                        if (pole[i - j, j] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[i - j, j] == "O")
+                        {
+                            pocetO += 1;
+                        }
+
+                        gameOver();
+
+                    }
+                }
+
+            }
+           
+           for(int x = 1; x < 10; x++)
+            {
+                int xplus = -1;
+                yminus += 1;
+               
+
+                for (int y = 0; y < 10 - yminus ; y++) { 
+                    xplus += 1;
+
+                    if (pole[x + xplus, 9 - xplus] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+
+                    if (pole[x + xplus, 9 - xplus] != "@")
+                    {
+                        if (pole[x + xplus, 9 - xplus] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[x + xplus, 9 - xplus] == "O")
+                        {
+                            pocetO += 1;
+                        }
+
+                        gameOver();
+
+
+
+
+
+                    }
+                }
+            }      
+
+
+
+
+        }
+            
+
+        public void checkDiagonalTwo()
+        {
+
+           
+
+            for(int y = 0; y < 10; y++)
+            {
+
+
+                pocetX = 0;
+                pocetO = 0;
+                yminusTwo = -1;
+                for (int x = 0; x < y + 1; x++)
+                {
+                    yminusTwo += 1;
+
+                    if (pole[9 - yminusTwo, y - yminusTwo] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+
+                    if (pole[9 - yminusTwo, y - yminusTwo] != "@")
+                    {
+                        if (pole[9 - yminusTwo, y - yminusTwo] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[9 - yminusTwo, y - yminusTwo] == "O")
+                        {
+                            pocetO += 1;
+                        }
+
+                        gameOver();
+
+                    }
+                }               
+            }
+
+            for (int x = 9; x > 0; x--)
+            {
+
+                pocetX = 0;
+                pocetO = 0;
+                for (int i = 0; i < (10 - x); i++)
+                {
+                    if (pole[i, x + i] == "@")
+                    {
+                        pocetX = 0;
+                        pocetO = 0;
+                    }
+
+                    if (pole[i, x + i] != "@")
+                    {
+                        if (pole[i, x + i] == "X")
+                        {
+                            pocetX += 1;
+
+                        }
+                        else if (pole[i, x + i] == "O")
+                        {
+                            pocetO += 1;
+                        }
+
+                        gameOver();
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(hra == false)
+            { 
+
+            for (int y = 0; y < 10; y++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+
+                pole[y, j] = Convert.ToString(vystup1.Text[j+ (12 * y)]);
+
+
+                }
+
+            }
+            /*
+            int celkemX = 0;
+            int celkemO = 0;
+
+            for (int y = 0; y < 10; y++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+
+                    if (pole[y, j] == "X")
+                    {
+                        celkemX += 1;
+                          
+
+                        }
+                    else if (pole[y, j] == "O")
+                    {
+                        celkemO += 1;
+                    }
+
+
+                }
+
+            }
+           */
+
+            if (rInt == 1)            
+        {             
+                                         
+          
+                             
+            label1.Text = "Hraje O";
+          
+
+           
+                rInt = 2;
+
+        }
+        else 
+        {          
+             label1.Text = "Hraje X";
+            
+                 rInt = 1;
+        }
+        
+
+            checkDiagonalTwo();
+            checkDiagonalOne();
+            checkColomns();
+            checkRows();
+            pocetX = 0;
+            pocetO = 0;
+           
+
+        }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (hra == true)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        pole[i, y] = "H";
+                    }
+                }
+                label2.Text = "Vyhry X: " + vyhryX;
+                label3.Text = "Vyhry O: " + vyhryO;
+                CheckKdoHraje();
+                vystup1.Text = "";
+                startGame();
+                hra = false;
+
+            }
+           
+            
+        }
+
+        
+    }
+}
+
+    
